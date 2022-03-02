71d0aba0edd25cb1f5e7d2469a328f6b907d4a7b
seungnyeong <seungnyeong@wemakeprice.com>
2022-03-02 16:32:19 +0900

    edit rest api

--- docs/docs.go
+++ docs/docs.go
@@ -268,7 +268,7 @@ const docTemplate_swagger = `{
 // SwaggerInfo_swagger holds exported Swagger Info so clients can modify it
 var SwaggerInfo_swagger = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "10.107.12.65:8081",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "wkms-alert module",
